a9d7eb954ecfacc7caa3c5169a1261931060e3f2
Noemie <noemie.deraedt@epitech.eu>
2019-03-29 13:37:46 +0100

    setenv detecting if variable name contains non-alphanumeric characters

--- src/setenv.c
+++ src/setenv.c
@@ -28,6 +28,18 @@ int find_equal(char *name)
     return 0;
 }
 
+int check_alphanum(char *input)
+{
+    for (int i = 0; i < my_strlen(input); i++) {
+        if ((input[i] < '0' || (input[i] > 'Z' && input[i] < 'a')
+        || input[i] > 'z') && input[i] != ' ' && input[i] != '\n') {
+            write(2, "setenv: Variable name must contain alphanumeric characters.\n", 61);
+            return 1;
+        }
+    }
+    return 0;
+}
+
 void new_env_var(char *input, char **my_env)
 {
     int a;
@@ -36,12 +48,13 @@ void new_env_var(char *input, char **my_env)
     int n = 0;
     char *name = malloc(sizeof(char) * (my_strlen(input) - 7));
 
-    for (k = 7; input[k]; k++) {
+    if (check_alphanum(input) == 1)
+        return;
+    for (k = 7; input[k]; k++, n++) {
         if (input[k] != ' ' && input[k] != '\n')
             name[n] = input[k];
         else
             name[n] = '=';
-        n++;
     }
     if (find_equal(name) == 84 && name[n - 1] == '=')
         name[n - 1] = '\0';
