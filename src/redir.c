77b5dc82b1fbb952c8cac8c2b66527f2cdf706f0
Noemie <noemie.deraedt@epitech.eu>
2019-04-19 14:42:46 +0200

    double redirect right functional

--- src/redir.c
+++ src/redir.c
@@ -16,18 +16,31 @@
 char **create_args(char *input)
 {
     int i = 0;
-    char **str = malloc(sizeof(char) * 2);
+    int k;
+    char **str = malloc(sizeof(char *) * (2 + 1));
 
     for (int j = 0; j < find_right_redirect(input) + 1; j++) {
-        for (int k = 0; i != -1; k++) {
-            str[k] = malloc(sizeof(char) * my_strlen(input));
+        for (k = 0; i != -1; k++) {
+            str[k] = malloc(sizeof(char) * (my_strlen(input) + 1));
             if (str[k] == NULL)
                 return NULL;
             transform_input(i, input, '>', str[k]);
             i = find_char(input, i + 1, '>');
         }
     }
+    str[2] = NULL;
+    str[1][my_strlen(str[1]) - 1] = '\0';
     return str;
+}
+
+int check_double_redir(char *input)
+{
+    for (int i = 0; input[i]; i++) {
+        if (input[i] == '>' && input[i + 1] && input[i + 1] == '>'
+        && input[i + 2] && input[i + 2] != '>')
+        return 1;
+    }
+    return 0;
 }
 
 int right_redir(char *input, char **env)
@@ -38,8 +51,10 @@ int right_redir(char *input, char **env)
 
     if (str == NULL)
         return 84;
-    str[1][my_strlen(str[1]) - 1] = '\0';
-    fd = open(str[1], O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
+    if (check_double_redir(input) == 0)
+        fd = open(str[1], O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
+    else
+        fd = open(str[1], O_CREAT | O_RDWR | O_APPEND, S_IRUSR | S_IWUSR);
     pid = fork();
     if (pid == 0) {
         dup2(fd, 1);
